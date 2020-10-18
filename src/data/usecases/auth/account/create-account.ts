98ebc6d690dbb6b6d74c768fe7b7a545304e624a
Allan Gaby <allan.gaby@gmail.com>
2020-10-18 16:41:21 -0300

    feat: ensure CreateAccount call SendMailAdapter with correct values

--- src/data/usecases/auth/account/create-account.ts
+++ src/data/usecases/auth/account/create-account.ts
@@ -4,7 +4,7 @@ import { SessionModel, SessionType } from '@/domain/models/auth'
 import { GetAccountByEmailRepository, CreateAccountRepository } from '@/data/repositories/auth/account'
 import { Hasher } from '@/data/protocols/criptography/hasher'
 import { CreateSessionRepository } from '@/data/repositories/auth/session'
-import { MailTemplateAdapter } from '@/data/protocols/comunication/mail'
+import { MailTemplateAdapter, SendMailAdapter } from '@/data/protocols/comunication/mail'
 
 export class DbCreateAccount implements CreateAccount {
   constructor (
@@ -13,7 +13,8 @@ export class DbCreateAccount implements CreateAccount {
     private readonly createAccountRepository: CreateAccountRepository,
     private readonly createSessionRepository: CreateSessionRepository,
     private readonly mailTemplateAdapter: MailTemplateAdapter,
-    private readonly mailFilePath: string
+    private readonly mailFilePath: string,
+    private readonly sendMailAdapter: SendMailAdapter
   ) {}
 
   async add (data: AddAccountDTO): Promise<SessionModel> {
@@ -35,9 +36,17 @@ export class DbCreateAccount implements CreateAccount {
         sessionId: session.id,
         name
       }
-      await this.mailTemplateAdapter.parse({
+      const mailContent = await this.mailTemplateAdapter.parse({
         filePath: this.mailFilePath,
         variables
+      })
+      await this.sendMailAdapter.sendMail({
+        to: {
+          name,
+          email
+        },
+        subject: `[Identity] - ${name}, sua conta foi criada com sucesso`,
+        content: mailContent
       })
       return session
     }
