43f0577a577c6e622a3ec5bef5b9542801d0ada5
Allan Gaby <allan.gaby@gmail.com>
2020-10-19 22:03:54 -0300

    feat: ensure DbUpdateAccount call MailTemplateAdapter with correct values

--- src/data/usecases/auth/account/update-account.ts
+++ src/data/usecases/auth/account/update-account.ts
@@ -4,13 +4,16 @@ import { UpdateAccountDTO } from '@/domain/dtos/auth/account'
 import { AccountModel, SessionType } from '@/domain/models/auth'
 import { UpdateAccount } from '@/domain/usecases/auth/account'
 import { CreateSessionRepository } from '@/data/repositories/auth/session'
+import { MailTemplateAdapter } from '@/data/protocols/comunication/mail'
 
 export class DbUpdateAccount implements UpdateAccount {
   constructor (
     private readonly getAccountByIdRepository: GetAccountByIdRepository,
     private readonly hasher: Hasher,
     private readonly updateAccountRepoitory: UpdateAccountRepository,
-    private readonly createSessionRepository: CreateSessionRepository
+    private readonly createSessionRepository: CreateSessionRepository,
+    private readonly mailTemplateAdapter: MailTemplateAdapter,
+    private readonly mailFilePath: string
   ) {}
 
   async update ({ id, name, email, password }: UpdateAccountDTO): Promise<AccountModel> {
@@ -29,10 +32,18 @@ export class DbUpdateAccount implements UpdateAccount {
         email_valided: emailValided
       })
       if (!emailValided) {
-        await this.createSessionRepository.add({
+        const session = await this.createSessionRepository.add({
           accountId: updatedAccount.id,
           type: SessionType.activeAccount,
           experied_at: new Date(new Date().getDate() + 1)
+        })
+        const variables = {
+          sessionId: session.id,
+          name
+        }
+        await this.mailTemplateAdapter.parse({
+          filePath: this.mailFilePath,
+          variables
         })
       }
     }
