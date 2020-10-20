3064c5aea5aa54ee83def1113152cbc49fb99922
Allan Gaby <allan.gaby@gmail.com>
2020-10-20 15:07:35 -0300

    feat: ensure DbSendMailActiveAccount call MailTemplateAdapter with correct values

--- src/data/usecases/auth/account/send-mail-active-account.ts
+++ src/data/usecases/auth/account/send-mail-active-account.ts
@@ -2,19 +2,30 @@ import { SendMailActiveAccountDTO } from '@/domain/dtos/auth/account'
 import { SendMailActiveAccount } from '@/domain/usecases/auth/account'
 import { SessionModel, SessionType } from '@/domain/models/auth'
 import { CreateSessionRepository } from '@/data/repositories/auth/session'
+import { MailTemplateAdapter } from '@/data/protocols/comunication/mail'
 
 export class DbSendMailActiveAccount implements SendMailActiveAccount {
   constructor (
-    private readonly createSessionRepository: CreateSessionRepository
+    private readonly createSessionRepository: CreateSessionRepository,
+    private readonly mailTemplateAdapter: MailTemplateAdapter,
+    private readonly mailFilePath: string
   ) {}
 
   async sendMail ({ accountId, email, name }: SendMailActiveAccountDTO): Promise<SessionModel> {
-    await this.createSessionRepository.add({
+    const session = await this.createSessionRepository.add({
       accountId,
       type: SessionType.activeAccount,
       experied_at: new Date(new Date().getDate() + 1)
     })
+    const variables = {
+      sessionId: session.id,
+      name
+    }
+    await this.mailTemplateAdapter.parse({
+      filePath: this.mailFilePath,
+      variables
+    })
 
-    return null
+    return session
   }
 }
