28d51ab41b2b2b0401ff31a337b97a0bb94de9c7
Allan Gaby <allan.gaby@gmail.com>
2020-10-21 17:26:47 -0300

    feat: ensure DbAuthenticationAccount call CreateSessionRepository with correct value

--- src/data/usecases/auth/account/authentication-account.ts
+++ src/data/usecases/auth/account/authentication-account.ts
@@ -1,12 +1,15 @@
 import { AuthenticationAccountDTO } from '@/domain/dtos/auth/account'
 import { AuthenticationAccount } from '@/domain/usecases/auth/account'
 import { GetAccountByEmailRepository } from '@/data/repositories/auth/account'
+import { CreateSessionRepository } from '@/data/repositories/auth/session'
 import { HashComparer, Encrypter } from '@/data/protocols/criptography'
+import { SessionType } from '@/domain/models/auth'
 
 export class DbAuthenticationAccount implements AuthenticationAccount {
   constructor (
     private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
+    private readonly createSessionRepository: CreateSessionRepository,
     private readonly encrypter: Encrypter
   ) {}
 
@@ -18,6 +21,11 @@ export class DbAuthenticationAccount implements AuthenticationAccount {
         hashedText: account.password
       })
       if (isCorrectPassword) {
+        await this.createSessionRepository.add({
+          accountId: account.id,
+          type: SessionType.authentication,
+          experied_at: new Date(new Date().getDate() + 1)
+        })
         return await this.encrypter.encrypt(account.id)
       }
     }
