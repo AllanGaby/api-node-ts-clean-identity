efa5c735f17046df6f4bd7c9d786e82059c27537
Allan Gaby <allan.gaby@gmail.com>
2020-10-21 15:38:37 -0300

    feat: ensure DbAuthenticateAccount call Encrypter with correct value

--- src/data/usecases/auth/account/authentication-account.ts
+++ src/data/usecases/auth/account/authentication-account.ts
@@ -1,21 +1,25 @@
 import { AuthenticationAccountDTO } from '@/domain/dtos/auth/account'
 import { AuthenticationAccount } from '@/domain/usecases/auth/account'
 import { GetAccountByEmailRepository } from '@/data/repositories/auth/account'
-import { HashComparer } from '@/data/protocols/criptography'
+import { HashComparer, Encrypter } from '@/data/protocols/criptography'
 
 export class DbAuthenticationAccount implements AuthenticationAccount {
   constructor (
     private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async authenticate ({ email, password }: AuthenticationAccountDTO): Promise<string> {
     const account = await this.getAccountByEmailRepository.getAccountByEmail(email)
     if (account) {
-      await this.hashComparer.compare({
+      const isCorrectPassword = await this.hashComparer.compare({
         payload: password,
         hashedText: account.password
       })
+      if (isCorrectPassword) {
+        await this.encrypter.encrypt(account.id)
+      }
     }
     return null
   }
