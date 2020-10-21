7128ce02c1f5b5102eaaf266f25feefe28d5e1ca
Allan Gaby <allan.gaby@gmail.com>
2020-10-21 18:13:03 -0300

    feat: ensure DbRequestRecoverPassword call CreateSessionRepository with correct value

--- src/data/usecases/auth/account/request-recover-password.ts
+++ src/data/usecases/auth/account/request-recover-password.ts
@@ -1,14 +1,24 @@
 import { RequestRecoverPasswordDTO } from '@/domain/dtos/auth/account'
 import { RequestRecoverPassword } from '@/domain/usecases/auth/account'
 import { GetAccountByEmailRepository } from '@/data/repositories/auth/account'
+import { CreateSessionRepository } from '@/data/repositories/auth/session'
+import { SessionType } from '@/domain/models/auth'
 
 export class DbRequestRecoverPassword implements RequestRecoverPassword {
   constructor (
-    private readonly getAccountByEmailRepository: GetAccountByEmailRepository
+    private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
+    private readonly createSessionRepository: CreateSessionRepository
   ) {}
 
   async request ({ email }: RequestRecoverPasswordDTO): Promise<boolean> {
-    await this.getAccountByEmailRepository.getAccountByEmail(email)
+    const accountByEmail = await this.getAccountByEmailRepository.getAccountByEmail(email)
+    if (accountByEmail) {
+      await this.createSessionRepository.add({
+        accountId: accountByEmail.id,
+        type: SessionType.recoverPassword,
+        experied_at: new Date(new Date().getDate() + 1)
+      })
+    }
     return false
   }
 }
