65e22d4292f9d03be7ccb1efe87eea1531027a41
Allan Gaby <allan.gaby@gmail.com>
2020-10-22 15:41:24 -0300

    feat: ensure RecoverPassword call HashCreator with correct value

--- src/data/usecases/auth/account/recover-password.ts
+++ src/data/usecases/auth/account/recover-password.ts
@@ -3,11 +3,13 @@ import { RecoverPasswordDTO } from '@/domain/dtos/auth/account'
 import { AccountModel, SessionType } from '@/domain/models/auth'
 import { GetSessionByIdRepository } from '@/data/repositories/auth/session'
 import { GetAccountByIdRepository } from '@/data/repositories/auth/account'
+import { HashCreator } from '@/data/protocols/criptography'
 
 export class DbRecoverPassword implements RecoverPassword {
   constructor (
     private readonly getSessionByIdRepository: GetSessionByIdRepository,
-    private readonly getAccountByIdRepository: GetAccountByIdRepository
+    private readonly getAccountByIdRepository: GetAccountByIdRepository,
+    private readonly hashCreator: HashCreator
   ) {}
 
   async recover ({ password, sessionId }: RecoverPasswordDTO): Promise<AccountModel> {
@@ -16,7 +18,8 @@ export class DbRecoverPassword implements RecoverPassword {
         (session.type === SessionType.recoverPassword) &&
         (session.experied_at > new Date()) &&
         (!session.deleted_at)) {
-      await this.getAccountByIdRepository.getAccountById(session.accountId)
+      const account = await this.getAccountByIdRepository.getAccountById(session.accountId)
+      account.password = await this.hashCreator.createHash(password)
     }
     return null
   }
