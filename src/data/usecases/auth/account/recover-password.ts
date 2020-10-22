a75040c8c440a4d43f466d6d664d30ad251b3e2e
Allan Gaby <allan.gaby@gmail.com>
2020-10-22 15:34:03 -0300

    feat: ensure RecoverPassword call GetAccountByIdRepository with correct value

--- src/data/usecases/auth/account/recover-password.ts
+++ src/data/usecases/auth/account/recover-password.ts
@@ -1,15 +1,23 @@
 import { RecoverPassword } from '@/domain/usecases/auth/account'
 import { RecoverPasswordDTO } from '@/domain/dtos/auth/account'
-import { AccountModel } from '@/domain/models/auth'
+import { AccountModel, SessionType } from '@/domain/models/auth'
 import { GetSessionByIdRepository } from '@/data/repositories/auth/session'
+import { GetAccountByIdRepository } from '@/data/repositories/auth/account'
 
 export class DbRecoverPassword implements RecoverPassword {
   constructor (
-    private readonly getSessionByIdRepository: GetSessionByIdRepository
+    private readonly getSessionByIdRepository: GetSessionByIdRepository,
+    private readonly getAccountByIdRepository: GetAccountByIdRepository
   ) {}
 
   async recover ({ password, sessionId }: RecoverPasswordDTO): Promise<AccountModel> {
-    await this.getSessionByIdRepository.getSessionById(sessionId)
+    const session = await this.getSessionByIdRepository.getSessionById(sessionId)
+    if ((session) &&
+        (session.type === SessionType.recoverPassword) &&
+        (session.experied_at > new Date()) &&
+        (!session.deleted_at)) {
+      await this.getAccountByIdRepository.getAccountById(session.accountId)
+    }
     return null
   }
 }
