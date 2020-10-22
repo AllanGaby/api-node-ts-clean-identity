1902e476996a6b7708507b91e75b13514b079ec9
Allan Gaby <allan.gaby@gmail.com>
2020-10-22 15:48:00 -0300

    feat: ensure RecoverPassword call UpdateAccountRepository with correct value

--- src/data/usecases/auth/account/recover-password.ts
+++ src/data/usecases/auth/account/recover-password.ts
@@ -2,14 +2,15 @@ import { RecoverPassword } from '@/domain/usecases/auth/account'
 import { RecoverPasswordDTO } from '@/domain/dtos/auth/account'
 import { AccountModel, SessionType } from '@/domain/models/auth'
 import { GetSessionByIdRepository } from '@/data/repositories/auth/session'
-import { GetAccountByIdRepository } from '@/data/repositories/auth/account'
+import { GetAccountByIdRepository, UpdateAccountRepository } from '@/data/repositories/auth/account'
 import { HashCreator } from '@/data/protocols/criptography'
 
 export class DbRecoverPassword implements RecoverPassword {
   constructor (
     private readonly getSessionByIdRepository: GetSessionByIdRepository,
     private readonly getAccountByIdRepository: GetAccountByIdRepository,
-    private readonly hashCreator: HashCreator
+    private readonly hashCreator: HashCreator,
+    private readonly updateAccountRepository: UpdateAccountRepository
   ) {}
 
   async recover ({ password, sessionId }: RecoverPasswordDTO): Promise<AccountModel> {
@@ -20,6 +21,7 @@ export class DbRecoverPassword implements RecoverPassword {
         (!session.deleted_at)) {
       const account = await this.getAccountByIdRepository.getAccountById(session.accountId)
       account.password = await this.hashCreator.createHash(password)
+      return await this.updateAccountRepository.update(account)
     }
     return null
   }
