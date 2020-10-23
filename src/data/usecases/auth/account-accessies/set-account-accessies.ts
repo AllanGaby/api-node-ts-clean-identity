1299aac93e7452e606dbbee6ed92682142405773
Allan Gaby <allan.gaby@gmail.com>
2020-10-23 20:51:39 -0300

    feat: ensure SetAccountAccessies call if UpdateAccountAccessiesRepository if not return null

--- src/data/usecases/auth/account-accessies/set-account-accessies.ts
+++ src/data/usecases/auth/account-accessies/set-account-accessies.ts
@@ -1,18 +1,20 @@
 import { SetAccountAccessies } from '@/domain/usecases/auth/account-accessies'
 import { AccountAccessiesModel } from '@/domain/models/auth'
-import { CreateAccountAccessiesRepository, GetAccountAccessiesByAccountIdRepository } from '@/data/repositories/auth/account-accessies'
+import { CreateAccountAccessiesRepository, GetAccountAccessiesByAccountIdRepository, UpdateAccountAccessiesRepository } from '@/data/repositories/auth/account-accessies'
 
 export class DbSetAccountAccessies implements SetAccountAccessies {
   constructor (
     private readonly getAccountAccessiesByAccountIdRepository: GetAccountAccessiesByAccountIdRepository,
-    private readonly createAccountAccessiesRepository: CreateAccountAccessiesRepository
+    private readonly createAccountAccessiesRepository: CreateAccountAccessiesRepository,
+    private readonly updateAccountAccessiesRepository: UpdateAccountAccessiesRepository
   ) {}
 
   async set (accountAccessies: AccountAccessiesModel): Promise<AccountAccessiesModel> {
     const accountAccessiesByAccountId = await this.getAccountAccessiesByAccountIdRepository.getAccountAccessiesByAccountId(accountAccessies.accountId)
     if (!accountAccessiesByAccountId) {
-      await this.createAccountAccessiesRepository.create(accountAccessies)
+      return await this.createAccountAccessiesRepository.create(accountAccessies)
+    } else {
+      return await this.updateAccountAccessiesRepository.update(accountAccessies)
     }
-    return null
   }
 }
