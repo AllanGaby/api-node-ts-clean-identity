42f24a47a83ace0e324c608332aa1dcb9dd335e0
Allan Gaby <allan.gaby@gmail.com>
2020-10-24 15:16:55 -0300

    feat: ensure ShowAccountBySession call GetSessionByIdRepository with correct value

--- src/data/usecases/auth/session/show-account-by-session.ts
+++ src/data/usecases/auth/session/show-account-by-session.ts
@@ -1,14 +1,19 @@
 import { AccountModel } from '@/domain/models/auth'
 import { ShowAccountBySession, ShowAccountBySessionDTO } from '@/domain/usecases/auth/session'
 import { Decrypter } from '@/data/protocols/criptography'
+import { GetSessionByIdRepository } from '@/data/repositories/auth/session'
 
 export class DbShowAccountBySession implements ShowAccountBySession {
   constructor (
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly getSessionByIdRepository: GetSessionByIdRepository
   ) {}
 
   async show ({ accessToken }: ShowAccountBySessionDTO): Promise<AccountModel> {
-    await this.decrypter.decrypt(accessToken)
+    const sessionId = await this.decrypter.decrypt(accessToken)
+    if (sessionId) {
+      await this.getSessionByIdRepository.getSessionById(sessionId)
+    }
     return null
   }
 }
