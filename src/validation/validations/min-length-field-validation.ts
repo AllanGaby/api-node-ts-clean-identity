4b084d9d596a2f10ec564521f1e407eab0420c2e
Allan Gaby <allan.gaby@gmail.com>
2020-10-26 10:19:40 -0300

    feat: ensure MinLengthFieldValidation return InvalidParamError if validation fails

---
+++ src/validation/validations/min-length-field-validation.ts
@@ -0,0 +1,15 @@
+import { Validation } from '@/validation/protocols/validation'
+import { InvalidParamError } from '@/validation/errors'
+
+export class MinLengthFieldValidation implements Validation {
+  constructor (
+    private readonly field: string,
+    private readonly minLength: number) {}
+
+  validate (input: any): Error {
+    const value = input[this.field] as string
+    if (value.length < this.minLength) {
+      return new InvalidParamError(this.field)
+    }
+  }
+}
