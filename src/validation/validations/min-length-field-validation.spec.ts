47d5a11281c8b4313bdc780004f40d6e829a7375
Allan Gaby <allan.gaby@gmail.com>
2020-10-26 10:20:21 -0300

    test: ensure MinLengthFieldValidation return InvalidParamError if validation fails

---
+++ src/validation/validations/min-length-field-validation.spec.ts
@@ -0,0 +1,34 @@
+import { MinLengthFieldValidation } from './min-length-field-validation'
+import { InvalidParamError } from '@/validation/errors'
+import faker from 'faker'
+
+interface sutTypes {
+  sut: MinLengthFieldValidation
+  field: string
+  minLength: number
+}
+
+const makeSut = (): sutTypes => {
+  const field = faker.database.column()
+  const minLength = faker.random.number({ min: 1, max: 20 })
+  const sut = new MinLengthFieldValidation(field, minLength)
+  return {
+    sut,
+    minLength,
+    field
+  }
+}
+
+describe('MinLengthFieldValidation', () => {
+  test('Should return InvalidParamError if validation fails', () => {
+    const { sut, field, minLength } = makeSut()
+    const error = sut.validate({ [field]: faker.random.alphaNumeric(minLength - 1) })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+
+  test('Should return null if validation succeed', () => {
+    const { sut, field, minLength } = makeSut()
+    const error = sut.validate({ [field]: faker.random.alphaNumeric(minLength) })
+    expect(error).toBeFalsy()
+  })
+})
