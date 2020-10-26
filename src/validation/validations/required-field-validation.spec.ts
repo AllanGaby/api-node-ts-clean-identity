a4754a64625f63cdac51ad849ae4b197faf5aa81
Allan Gaby <allan.gaby@gmail.com>
2020-10-26 10:05:40 -0300

    test: ensure RequiredFieldValidation return MissingParamError if required field not found

---
+++ src/validation/validations/required-field-validation.spec.ts
@@ -0,0 +1,31 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '@/validation/errors'
+import faker from 'faker'
+
+interface sutTypes {
+  sut: RequiredFieldValidation
+  field: string
+}
+
+const makeSut = (): sutTypes => {
+  const field = faker.database.column()
+  const sut = new RequiredFieldValidation(field)
+  return {
+    sut,
+    field
+  }
+}
+
+describe('RequiredFieldValidation', () => {
+  test('Should return MissingParamError if required field not fould', () => {
+    const { sut, field } = makeSut()
+    const error = sut.validate({ invalidField: faker.random.words() })
+    expect(error).toEqual(new MissingParamError(field))
+  })
+
+  test('Should return null if fill required field', () => {
+    const { sut, field } = makeSut()
+    const error = sut.validate({ [field]: faker.random.words() })
+    expect(error).toBeFalsy()
+  })
+})
