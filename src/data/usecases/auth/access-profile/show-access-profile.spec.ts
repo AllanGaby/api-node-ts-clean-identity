58eebb3df7207303e7e8570782cf59367270cb38
Allan Gaby <allan.gaby@gmail.com>
2020-10-31 16:35:17 -0300

    test: ensure ShowAccessProfile return correc value

--- src/data/usecases/auth/access-profile/show-access-profile.spec.ts
+++ src/data/usecases/auth/access-profile/show-access-profile.spec.ts
@@ -1,5 +1,5 @@
 import { DbShowAccessProfile } from './show-access-profile'
-import { GetAccessProfileByIdRepositorySpy } from '@/data/test'
+import { GetAccessProfileByIdRepositorySpy, mockAccessProfileModel, throwError } from '@/data/test'
 import faker from 'faker'
 
 interface sutTypes {
@@ -22,5 +22,28 @@ describe('DbShowAccessProfile', () => {
     const id = faker.random.uuid()
     await sut.show(id)
     expect(getAccessProfileByIdRepositorySpy.params).toEqual(id)
+  })
+
+  test('Should throw if GetAccessProfileByIdRepository throws', async () => {
+    const { sut, getAccessProfileByIdRepositorySpy } = makeSut()
+    jest.spyOn(getAccessProfileByIdRepositorySpy, 'getAccessProfileById').mockImplementationOnce(throwError)
+    const promise = sut.show(faker.random.uuid())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return null if GetAccessProfileByIdRepository return null', async () => {
+    const { sut, getAccessProfileByIdRepositorySpy } = makeSut()
+    getAccessProfileByIdRepositorySpy.accessProfile = null
+    const accessProfile = await sut.show(faker.random.uuid())
+    expect(accessProfile).toBeFalsy()
+  })
+
+  test('Should return AccessProfile if GetAccessProfileByIdRepository return an AccessProfile', async () => {
+    const { sut, getAccessProfileByIdRepositorySpy } = makeSut()
+    getAccessProfileByIdRepositorySpy.accessProfile = mockAccessProfileModel()
+    const accessProfile = await sut.show(faker.random.uuid())
+    expect(accessProfile.id).toBeTruthy()
+    expect(accessProfile.title).toEqual(getAccessProfileByIdRepositorySpy.accessProfile.title)
+    expect(accessProfile.listAccount).toEqual(getAccessProfileByIdRepositorySpy.accessProfile.listAccount)
   })
 })
