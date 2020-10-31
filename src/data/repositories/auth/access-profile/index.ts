43072c0c0f2b063edf79647ed03f1641a426eeb2
Allan Gaby <allan.gaby@gmail.com>
2020-10-31 15:32:30 -0300

    feat: ensure CreateAccessProfile call CreateAccessProfileRepository with correct values

--- src/data/repositories/auth/access-profile/index.ts
+++ src/data/repositories/auth/access-profile/index.ts
@@ -1,1 +1,2 @@
 export * from './get-access-profile-by-title'
+export * from './create-access-profile'
--- src/data/usecases/auth/access-profile/create-access-profile.ts
+++ src/data/usecases/auth/access-profile/create-access-profile.ts
@@ -1,14 +1,18 @@
 import { AccessProfileModel } from '@/domain/models/auth'
 import { CreateAccessProfile, CreateAccessProfileDTO } from '@/domain/usecases/auth/access-profile'
-import { GetAccessProfileByTitleRepository } from '@/data/repositories/auth/access-profile'
+import { GetAccessProfileByTitleRepository, CreateAccessProfileRepository } from '@/data/repositories/auth/access-profile'
 
 export class DbCreateAccessProfile implements CreateAccessProfile {
   constructor (
-    private readonly getAccessProfileByTitleRepository: GetAccessProfileByTitleRepository
+    private readonly getAccessProfileByTitleRepository: GetAccessProfileByTitleRepository,
+    private readonly createAccessProfileRepository: CreateAccessProfileRepository
   ) {}
 
-  async create (accessProfile: CreateAccessProfileDTO): Promise<AccessProfileModel> {
-    await this.getAccessProfileByTitleRepository.getAccessProfileByTitle(accessProfile.title)
+  async create (accessProfileDTO: CreateAccessProfileDTO): Promise<AccessProfileModel> {
+    const accessProfileByTitle = await this.getAccessProfileByTitleRepository.getAccessProfileByTitle(accessProfileDTO.title)
+    if (!accessProfileByTitle) {
+      return await this.createAccessProfileRepository.create(accessProfileDTO)
+    }
     return null
   }
 }
