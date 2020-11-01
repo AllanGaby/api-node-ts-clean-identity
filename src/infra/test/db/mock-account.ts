24dda95db189ab0c55e328d98cdf8c749dce477d
Allan Gaby <allan.gaby@gmail.com>
2020-11-01 15:38:49 -0300

    test: ensure MemoryAccountRepository Update method return correct value

--- src/infra/test/db/mock-account.ts
+++ src/infra/test/db/mock-account.ts
@@ -1,8 +1,17 @@
-import { CreateAccountModel } from '@/data/repositories/auth/account'
+import { CreateAccountModel, UpdateAccountModel } from '@/data/repositories/auth/account'
 import faker from 'faker'
 
 export const mockCreateAccountModel = (): CreateAccountModel => ({
   email: faker.internet.email(),
   name: faker.name.findName(),
   password: faker.internet.password()
+})
+
+export const mockUpdateAccountModel = (emailValided: boolean = true, accessProfileId: string = undefined): UpdateAccountModel => ({
+  id: faker.random.uuid(),
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  email_valided: emailValided,
+  accessProfileId
 })
