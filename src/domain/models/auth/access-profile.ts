0c13de62adec89d49d7dd9775c32df16c89b6cf8
Allan Gaby <allan.gaby@gmail.com>
2020-11-02 16:18:11 -0300

    refactor: add create_at and updated_at in AccessProfileModel

--- src/domain/models/auth/access-profile.ts
+++ src/domain/models/auth/access-profile.ts
@@ -2,4 +2,6 @@ export interface AccessProfileModel {
   id: string
   title: string
   listAccount: boolean
+  created_at: Date
+  updated_at: Date
 }
--- src/domain/usecases/auth/access-profile/create-access-profile.ts
+++ src/domain/usecases/auth/access-profile/create-access-profile.ts
@@ -1,6 +1,9 @@
 import { AccessProfileModel } from '@/domain/models/auth'
 
-export type CreateAccessProfileDTO = Omit<AccessProfileModel, 'id'>
+export interface CreateAccessProfileDTO {
+  title: string
+  listAccount: boolean
+}
 
 export interface CreateAccessProfile {
   create: (accessProfile: CreateAccessProfileDTO) => Promise<AccessProfileModel>
