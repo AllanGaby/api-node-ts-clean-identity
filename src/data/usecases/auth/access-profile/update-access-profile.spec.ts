fc580cc3d0f93d8a7e315fb8c0896119193a9d43
Allan Gaby <allan.gaby@gmail.com>
2020-11-02 16:52:55 -0300

    test: ensure UpdateAccessProfile call UpdateAccessProfileRepository with correct values

--- src/data/usecases/auth/access-profile/update-access-profile.spec.ts
+++ src/data/usecases/auth/access-profile/update-access-profile.spec.ts
@@ -1,17 +1,20 @@
 import { DbUpdateAccessProfile } from './update-access-profile'
-import { GetAccessProfileByIdRepositorySpy, mockUpdateAccessProfileModel, throwError } from '@/data/test'
+import { GetAccessProfileByIdRepositorySpy, mockUpdateAccessProfileModel, throwError, UpdateAccessProfileRepositorySpy } from '@/data/test'
 
 interface sutTypes {
   sut: DbUpdateAccessProfile
   getAccessProfileByIdRepositorySpy: GetAccessProfileByIdRepositorySpy
+  updateAccessProfileRepositorySpy: UpdateAccessProfileRepositorySpy
 }
 
 const makeSut = (): sutTypes => {
   const getAccessProfileByIdRepositorySpy = new GetAccessProfileByIdRepositorySpy()
-  const sut = new DbUpdateAccessProfile(getAccessProfileByIdRepositorySpy)
+  const updateAccessProfileRepositorySpy = new UpdateAccessProfileRepositorySpy()
+  const sut = new DbUpdateAccessProfile(getAccessProfileByIdRepositorySpy, updateAccessProfileRepositorySpy)
   return {
     sut,
-    getAccessProfileByIdRepositorySpy
+    getAccessProfileByIdRepositorySpy,
+    updateAccessProfileRepositorySpy
   }
 }
 
@@ -33,6 +36,20 @@ describe('DbUpdateAccessProfile', () => {
   test('Should throw if GetAccessProfileByIdRepository return null', async () => {
     const { sut, getAccessProfileByIdRepositorySpy } = makeSut()
     getAccessProfileByIdRepositorySpy.accessProfile = null
+    const promise = sut.update(mockUpdateAccessProfileModel())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call UpdateAccessProfileRepository with correct value', async () => {
+    const { sut, updateAccessProfileRepositorySpy } = makeSut()
+    const updateAccessProfileModel = mockUpdateAccessProfileModel()
+    await sut.update(updateAccessProfileModel)
+    expect(updateAccessProfileRepositorySpy.params).toEqual(updateAccessProfileModel)
+  })
+
+  test('Should throw if UpdateAccessProfileRepository throws', async () => {
+    const { sut, updateAccessProfileRepositorySpy } = makeSut()
+    jest.spyOn(updateAccessProfileRepositorySpy, 'update').mockImplementationOnce(throwError)
     const promise = sut.update(mockUpdateAccessProfileModel())
     await expect(promise).rejects.toThrow()
   })
