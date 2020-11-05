94eb986085277cd90c271e8235dfc8856f16d364
Allan Gaby <allan.gaby@gmail.com>
2020-11-04 21:38:43 -0300

    feat: create criptografy infra implementations with bcrypt and jsonwebtoken libs

---
+++ src/infra/criptografy/bcrypt-hasher-adapter.ts
@@ -0,0 +1,14 @@
+import { HashCreator, HashComparer, ComparerDTO } from '@/data/protocols/criptography'
+import bcrypt from 'bcrypt'
+
+export class BCrypterHasherAdapter implements HashCreator, HashComparer {
+  constructor (private readonly salt: number = 12) {}
+
+  async createHash (payload: string): Promise<string> {
+    return await bcrypt.hash(payload, this.salt)
+  }
+
+  async compare (data: ComparerDTO): Promise<boolean> {
+    return await bcrypt.compare(data.payload, data.hashedText)
+  }
+}
---
+++ src/infra/criptografy/jsonwebtoken-encrypter-adapter.ts
@@ -0,0 +1,27 @@
+import { Encrypter, Decrypter } from '@/data/protocols/criptography'
+import { sign, verify } from 'jsonwebtoken'
+
+interface TokenPayload {
+  ait: number
+  exp: number
+  sub: string
+}
+
+export class JWTEncrypterAdapter implements Encrypter, Decrypter {
+  constructor (
+    private readonly secret: string,
+    private readonly deadlineInDays: number = 1
+  ) {}
+
+  async encrypt (plainText: string): Promise<string> {
+    return sign({}, this.secret, {
+      subject: plainText,
+      expiresIn: `${this.deadlineInDays}d`
+    })
+  }
+
+  async decrypt (encryptedText: string): Promise<string> {
+    const decoded = verify(encryptedText, this.secret) as TokenPayload
+    return decoded.sub
+  }
+}
