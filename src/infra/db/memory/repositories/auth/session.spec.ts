32d4b19ea7a863f97bc9e0e567cd9a12134fd52b
Allan Gaby <allan.gaby@gmail.com>
2020-11-12 21:20:28 -0300

    test: ensure MemorySessionRepository return correct values

--- src/infra/db/memory/repositories/auth/session.spec.ts
+++ src/infra/db/memory/repositories/auth/session.spec.ts
@@ -1,5 +1,7 @@
 import { MemorySessionRepository } from './session'
 import { mockCreateSessionModel } from '@/infra/test/db/memory/auth'
+import faker from 'faker'
+import { mockSessionModel } from '@/data/test'
 
 interface sutType {
   sut: MemorySessionRepository
@@ -21,5 +23,42 @@ describe('MemorySessionRepository Create Method', () => {
     expect(createdSession.experied_at).toBe(createSessionModel.experied_at)
     expect(createdSession.accountId).toBe(createSessionModel.accountId)
     expect(createdSession.type).toBe(createSessionModel.type)
+  })
+})
+
+describe('MemorySessionRepository GetSessionById Method', () => {
+  test('Should return null if session not found', async () => {
+    const { sut } = makeSut()
+    const sessionById = await sut.getSessionById(faker.random.uuid())
+    expect(sessionById).toBeFalsy()
+  })
+
+  test('Should return an session with correct id', async () => {
+    const { sut } = makeSut()
+    const createdSession = await sut.create(mockCreateSessionModel())
+    const sessionById = await sut.getSessionById(createdSession.id)
+    expect(sessionById.id).toBe(createdSession.id)
+  })
+})
+
+describe('MemorySessionRepository GetSessionById Method', () => {
+  test('Should return null if session not found', async () => {
+    const { sut } = makeSut()
+    const session = mockSessionModel()
+    const beforeSession = await sut.getSessionById(session.id)
+    expect(beforeSession).toBeFalsy()
+    await sut.delete(beforeSession)
+    const afterSession = await sut.getSessionById(session.id)
+    expect(afterSession).toBeFalsy()
+  })
+
+  test('Should change session list if session found', async () => {
+    const { sut } = makeSut()
+    const createdSession = await sut.create(mockCreateSessionModel())
+    const beforeSession = await sut.getSessionById(createdSession.id)
+    expect(beforeSession).toEqual(beforeSession)
+    await sut.delete(beforeSession)
+    const afterSession = await sut.getSessionById(createdSession.id)
+    expect(afterSession).toBeFalsy()
   })
 })
--- src/infra/test/db/memory/auth/mock-session.ts
+++ src/infra/test/db/memory/auth/mock-session.ts
@@ -1,9 +1,18 @@
 import { CreateSessionModel } from '@/data/repositories/auth/session'
-import { SessionType } from '@/domain/models/auth'
+import { SessionType, SessionModel } from '@/domain/models/auth'
 import faker from 'faker'
 
 export const mockCreateSessionModel = (type: SessionType = SessionType.authentication): CreateSessionModel => ({
   accountId: faker.random.uuid(),
   type,
   experied_at: new Date()
+})
+
+export const mockSessionModel = (type: SessionType = SessionType.authentication): SessionModel => ({
+  accountId: faker.random.uuid(),
+  created_at: faker.date.past(),
+  updated_at: faker.date.past(),
+  experied_at: faker.date.future(),
+  id: faker.random.uuid(),
+  type
 })
