c7a1752b74448a0303ca460ea165e0e4d9d9ff55
Allan Gaby <allan.gaby@gmail.com>
2020-11-14 16:32:56 -0300

    feat: ensure CreateAccountController call CreateAccount usecase with correct values

--- src/presentation/controllers/auth/account/create-account.ts
+++ src/presentation/controllers/auth/account/create-account.ts
@@ -1,18 +1,26 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { ValidationComposite } from '@/validation/validations'
 import { badRequest, serverError } from '@/presentation/helpers'
+import { CreateAccount } from '@/domain/usecases/auth/account'
 
 export class CreateAccountController implements Controller {
   constructor (
-    private readonly createAccountValidations: ValidationComposite
+    private readonly validations: ValidationComposite,
+    private readonly createAccount: CreateAccount
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
-      const validationErrors = this.createAccountValidations.validate(request.body)
+      const validationErrors = this.validations.validate(request.body)
       if (validationErrors) {
         return badRequest(validationErrors)
       }
+      const { name, email, password } = request.body
+      await this.createAccount.create({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError(error)
     }
