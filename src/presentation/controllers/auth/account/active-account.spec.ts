c9fbb6c9b63b39b75c7e4280c829ca0fea763815
Allan Gaby <allan.gaby@gmail.com>
2020-11-16 07:09:39 -0300

    test: ensure ActiveAccountController return BadRequest if validation return an error

--- src/presentation/controllers/auth/account/active-account.spec.ts
+++ src/presentation/controllers/auth/account/active-account.spec.ts
@@ -1,6 +1,7 @@
 import { ActiveAccountController } from './active-account'
 import { ValidationCompositeSpy } from '@/validation/test'
 import { ActiveAccountSpy, mockActiveAccountRequest } from '@/presentation/test/auth'
+import { badRequest } from '@/presentation/helpers'
 
 interface sutTypes {
   sut: ActiveAccountController
@@ -26,5 +27,12 @@ describe('ActiveAccountController', () => {
     const request = mockActiveAccountRequest()
     await sut.handle(request)
     expect(validationCompositeSpy.params).toEqual(request.body)
+  })
+
+  test('Should return BadRequest if validation is fails', async () => {
+    const { sut, validationCompositeSpy } = makeSut()
+    validationCompositeSpy.error = new Error('Validation error')
+    const result = await sut.handle(mockActiveAccountRequest())
+    expect(result).toEqual(badRequest(validationCompositeSpy.error))
   })
 })
