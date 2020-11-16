1caf974d9d2bbdf281a65ab0b295b51887ba516a
Allan Gaby <allan.gaby@gmail.com>
2020-11-16 07:48:07 -0300

    feat: ensure AuthenticationAccountController return ServerError if AuthenticateAccount throws

--- src/presentation/controllers/auth/account/authentication-account.ts
+++ src/presentation/controllers/auth/account/authentication-account.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '@/presentation/helpers'
+import { badRequest, serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { ValidationComposite } from '@/validation/validations'
 import { AuthenticationAccount } from '@/domain/usecases/auth/account'
@@ -10,15 +10,19 @@ export class AuthenticationAccountController implements Controller {
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const validationErrors = this.validations.validate(request.body)
-    if (validationErrors) {
-      return badRequest(validationErrors)
+    try {
+      const validationErrors = this.validations.validate(request.body)
+      if (validationErrors) {
+        return badRequest(validationErrors)
+      }
+      const { email, password } = request.body
+      await this.authenticationAccount.authenticate({
+        email,
+        password
+      })
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    const { email, password } = request.body
-    await this.authenticationAccount.authenticate({
-      email,
-      password
-    })
-    return null
   }
 }
