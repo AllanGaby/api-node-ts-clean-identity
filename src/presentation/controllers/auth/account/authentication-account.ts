149bdee46d7cf400d4d0f851caa61a19846b52a5
Allan Gaby <allan.gaby@gmail.com>
2020-11-16 07:44:37 -0300

    feat: ensure AuthenticationAccountController call AuthenticateAccount with correct values

--- src/presentation/controllers/auth/account/authentication-account.ts
+++ src/presentation/controllers/auth/account/authentication-account.ts
@@ -1,10 +1,12 @@
 import { badRequest } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { ValidationComposite } from '@/validation/validations'
+import { AuthenticationAccount } from '@/domain/usecases/auth/account'
 
 export class AuthenticationAccountController implements Controller {
   constructor (
-    private readonly validations: ValidationComposite
+    private readonly validations: ValidationComposite,
+    private readonly authenticationAccount: AuthenticationAccount
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
@@ -12,6 +14,11 @@ export class AuthenticationAccountController implements Controller {
     if (validationErrors) {
       return badRequest(validationErrors)
     }
+    const { email, password } = request.body
+    await this.authenticationAccount.authenticate({
+      email,
+      password
+    })
     return null
   }
 }
