40f6eba87ee157a40f6ab1ce6e43b8b93c61dc0d
Allan Gaby <allan.gaby@gmail.com>
2020-11-16 07:15:33 -0300

    feat: ensure ActiveAccountController call ActiveAccount with correct values

--- src/presentation/controllers/auth/account/active-account.ts
+++ src/presentation/controllers/auth/account/active-account.ts
@@ -1,15 +1,21 @@
 import { badRequest } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { ValidationComposite } from '@/validation/validations'
+import { ActiveAccount } from '@/domain/usecases/auth/account'
 
 export class ActiveAccountController implements Controller {
-  constructor (private readonly validations: ValidationComposite) {}
+  constructor (
+    private readonly validations: ValidationComposite,
+    private readonly activeAccount: ActiveAccount
+  ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     const validationErrors = this.validations.validate(request.body)
     if (validationErrors) {
       return badRequest(validationErrors)
     }
+    const { sessionId } = request.body
+    await this.activeAccount.active({ sessionId })
     return null
   }
 }
