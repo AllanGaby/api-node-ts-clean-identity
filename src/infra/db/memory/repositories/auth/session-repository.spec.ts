921f9f3a269dc5b9b2cdf8eca2fb935b1ce3a51a
Allan Gaby <allan.gaby@gmail.com>
2020-11-23 07:21:42 -0300

    refactor: group repository method tests in unique describe

--- src/infra/db/memory/repositories/auth/session-repository.spec.ts
+++ src/infra/db/memory/repositories/auth/session-repository.spec.ts
@@ -14,51 +14,53 @@ const makeSut = (): sutType => {
   }
 }
 
-describe('MemorySessionRepository Create Method', () => {
-  test('Should return an new session with correct values', async () => {
-    const { sut } = makeSut()
-    const createSessionModel = mockCreateSessionModel()
-    const createdSession = await sut.create(createSessionModel)
-    expect(createdSession.id).toBeTruthy()
-    expect(createdSession.experied_at).toBe(createSessionModel.experied_at)
-    expect(createdSession.account_id).toBe(createSessionModel.account_id)
-    expect(createdSession.type).toBe(createSessionModel.type)
-  })
-})
-
-describe('MemorySessionRepository GetSessionById Method', () => {
-  test.only('Should return null if session not found', async () => {
-    const { sut } = makeSut()
-    const sessionById = await sut.getSessionById(faker.random.uuid())
-    expect(sessionById).toBeFalsy()
+describe('MemorySessionRepository', () => {
+  describe('Create Method', () => {
+    test('Should return an new session with correct values', async () => {
+      const { sut } = makeSut()
+      const createSessionModel = mockCreateSessionModel()
+      const createdSession = await sut.create(createSessionModel)
+      expect(createdSession.id).toBeTruthy()
+      expect(createdSession.experied_at).toBe(createSessionModel.experied_at)
+      expect(createdSession.account_id).toBe(createSessionModel.account_id)
+      expect(createdSession.type).toBe(createSessionModel.type)
+    })
   })
 
-  test('Should return an session with correct id', async () => {
-    const { sut } = makeSut()
-    const createdSession = await sut.create(mockCreateSessionModel())
-    const sessionById = await sut.getSessionById(createdSession.id)
-    expect(sessionById.id).toBe(createdSession.id)
-  })
-})
+  describe('GetSessionById Method', () => {
+    test('Should return null if session not found', async () => {
+      const { sut } = makeSut()
+      const sessionById = await sut.getSessionById(faker.random.uuid())
+      expect(sessionById).toBeFalsy()
+    })
 
-describe('MemorySessionRepository GetSessionById Method', () => {
-  test('Should return null if session not found', async () => {
-    const { sut } = makeSut()
-    const session = mockSessionModel()
-    const beforeSession = await sut.getSessionById(session.id)
-    expect(beforeSession).toBeFalsy()
-    await sut.delete(beforeSession)
-    const afterSession = await sut.getSessionById(session.id)
-    expect(afterSession).toBeFalsy()
+    test('Should return an session with correct id', async () => {
+      const { sut } = makeSut()
+      const createdSession = await sut.create(mockCreateSessionModel())
+      const sessionById = await sut.getSessionById(createdSession.id)
+      expect(sessionById.id).toBe(createdSession.id)
+    })
   })
 
-  test('Should change session list if session found', async () => {
-    const { sut } = makeSut()
-    const createdSession = await sut.create(mockCreateSessionModel())
-    const beforeSession = await sut.getSessionById(createdSession.id)
-    expect(beforeSession).toEqual(beforeSession)
-    await sut.delete(beforeSession)
-    const afterSession = await sut.getSessionById(createdSession.id)
-    expect(afterSession).toBeFalsy()
+  describe('GetSessionById Method', () => {
+    test('Should return null if session not found', async () => {
+      const { sut } = makeSut()
+      const session = mockSessionModel()
+      const beforeSession = await sut.getSessionById(session.id)
+      expect(beforeSession).toBeFalsy()
+      await sut.delete(beforeSession)
+      const afterSession = await sut.getSessionById(session.id)
+      expect(afterSession).toBeFalsy()
+    })
+
+    test('Should change session list if session found', async () => {
+      const { sut } = makeSut()
+      const createdSession = await sut.create(mockCreateSessionModel())
+      const beforeSession = await sut.getSessionById(createdSession.id)
+      expect(beforeSession).toEqual(beforeSession)
+      await sut.delete(beforeSession)
+      const afterSession = await sut.getSessionById(createdSession.id)
+      expect(afterSession).toBeFalsy()
+    })
   })
 })
