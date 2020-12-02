13b0f29607bccca3e6d43e91b57748592ba6a21e
Allan Gaby <allan.gaby@gmail.com>
2020-12-02 07:40:27 -0300

    feat: ensure UpdateAccountValidationFactory return correct validations

---
+++ src/main/factories/validations/auth/update-account-factory.ts
@@ -0,0 +1,24 @@
+import { ValidationComposite, ValidationBuilder } from '@/validation/validations'
+import { EmailValidator } from '@/validation/protocols'
+
+export interface UpdateAccountValidationConfig {
+  minLengthName: number
+  minLengthPassword: number
+  emailValidator: EmailValidator
+}
+
+export const makeUpdateAccountValidation = ({ minLengthName, minLengthPassword, emailValidator }: UpdateAccountValidationConfig): ValidationComposite => {
+  const idValidation = ValidationBuilder.field('id').required()
+  const nameValidation = ValidationBuilder.field('name').min(minLengthName)
+  const emailValidation = ValidationBuilder.field('email').email(emailValidator)
+  const passwordValidation = ValidationBuilder.field('password').min(minLengthPassword)
+  const passwordConfirmationValidation = ValidationBuilder.field('password_confirmation').min(minLengthPassword).sameAs('password')
+
+  return new ValidationComposite([
+    ...idValidation.build(),
+    ...nameValidation.build(),
+    ...emailValidation.build(),
+    ...passwordValidation.build(),
+    ...passwordConfirmationValidation.build()
+  ])
+}
