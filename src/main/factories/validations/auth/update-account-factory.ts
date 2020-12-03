6c9ddd98a87ebef03ec03b2af781e5c5fa965555
Allan Gaby <allan.gaby@gmail.com>
2020-12-03 07:18:09 -0300

    feat: ensure UpdateAccountController valid account id

--- src/main/factories/validations/auth/update-account-factory.ts
+++ src/main/factories/validations/auth/update-account-factory.ts
@@ -8,12 +8,14 @@ export interface UpdateAccountValidationConfig {
 }
 
 export const makeUpdateAccountValidation = ({ minLengthName, minLengthPassword, emailValidator }: UpdateAccountValidationConfig): ValidationComposite => {
+  const idValidation = ValidationBuilder.field('account.id').required()
   const nameValidation = ValidationBuilder.field('name').min(minLengthName)
   const emailValidation = ValidationBuilder.field('email').email(emailValidator)
   const passwordValidation = ValidationBuilder.field('password').min(minLengthPassword)
   const passwordConfirmationValidation = ValidationBuilder.field('password_confirmation').min(minLengthPassword).sameAs('password')
 
   return new ValidationComposite([
+    ...idValidation.build(),
     ...nameValidation.build(),
     ...emailValidation.build(),
     ...passwordValidation.build(),
