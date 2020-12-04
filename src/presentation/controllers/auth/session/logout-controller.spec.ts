7830469f8992992c9dc9c2561b4147387c97d803
Allan Gaby <allan.gaby@gmail.com>
2020-12-04 15:15:00 -0300

    test: ensure LogoutController return Forbidden if Logout return InvalidCredentialError

--- src/presentation/controllers/auth/session/logout-controller.spec.ts
+++ src/presentation/controllers/auth/session/logout-controller.spec.ts
@@ -1,7 +1,8 @@
 import { LogoutController } from './logout-controller'
 import { LogoutSpy, mockLogoutRequest } from '@/presentation/test/auth'
-import { serverError } from '@/presentation/helpers'
+import { forbidden, serverError } from '@/presentation/helpers'
 import { throwError } from '@/data/test'
+import { InvalidCredentialsError } from '@/data/errors'
 
 interface sutTypes {
   sut: LogoutController
@@ -30,5 +31,12 @@ describe('LogoutController', () => {
     jest.spyOn(logoutSpy, 'logout').mockImplementationOnce(throwError)
     const result = await sut.handle(mockLogoutRequest())
     expect(result).toEqual(serverError(new Error('')))
+  })
+
+  test('Should return Forbidden if Logount return InvalidCredentialError', async () => {
+    const { sut, logoutSpy } = makeSut()
+    jest.spyOn(logoutSpy, 'logout').mockImplementationOnce(() => { throw new InvalidCredentialsError() })
+    const result = await sut.handle(mockLogoutRequest())
+    expect(result).toEqual(forbidden(new InvalidCredentialsError()))
   })
 })
