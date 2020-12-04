f6dce4a0febd0003207c1615914ecfde47ac4610
Allan Gaby <allan.gaby@gmail.com>
2020-12-04 13:41:35 -0300

    test: ensure DbLogout throws if GetSessionByIdRepository throws

--- src/data/usecases/auth/session/logout.spec.ts
+++ src/data/usecases/auth/session/logout.spec.ts
@@ -1,5 +1,6 @@
 import { DbLogout } from './logout'
 import { GetSessionByIdRepositorySpy, mockLogoutDTO } from '@/data/test/auth/session'
+import { throwError } from '@/data/test'
 
 interface sutTypes {
   sut: DbLogout
@@ -21,5 +22,12 @@ describe('DbLogout', () => {
     const request = mockLogoutDTO()
     await sut.logout(request)
     expect(getSessionByIdRepositorySpy.sessionId).toEqual(request.sessionId)
+  })
+
+  test('Should throw if GetSessionByIdRepository throws', async () => {
+    const { sut, getSessionByIdRepositorySpy } = makeSut()
+    jest.spyOn(getSessionByIdRepositorySpy, 'getSessionById').mockImplementationOnce(throwError)
+    const promise = sut.logout(mockLogoutDTO())
+    await expect(promise).rejects.toThrow()
   })
 })
