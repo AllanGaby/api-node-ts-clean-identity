5b1064cefe34ff529db5872c144d1dc035254974
Allan Gaby <allan.gaby@gmail.com>
2020-12-04 13:49:23 -0300

    feat: ensure DbLogout call DeleteSessionRepository with correct value

--- src/data/usecases/auth/session/logout.ts
+++ src/data/usecases/auth/session/logout.ts
@@ -1,16 +1,19 @@
 import { InvalidCredentialsError } from '@/data/errors'
-import { GetSessionByIdRepository } from '@/data/repositories/auth'
+import { DeleteSessionRepository, GetSessionByIdRepository } from '@/data/repositories/auth'
 import { Logout, LogoutDTO } from '@/domain/usecases/auth/session'
 
 export class DbLogout implements Logout {
   constructor (
-    private readonly getSessionByIdRepository: GetSessionByIdRepository
+    private readonly getSessionByIdRepository: GetSessionByIdRepository,
+    private readonly deleteSessionRepository: DeleteSessionRepository
   ) {}
 
   async logout ({ sessionId }: LogoutDTO): Promise<void> {
     const session = await this.getSessionByIdRepository.getSessionById(sessionId)
     if (!session) {
       throw new InvalidCredentialsError()
+    } else {
+      this.deleteSessionRepository.delete(session)
     }
   }
 }
