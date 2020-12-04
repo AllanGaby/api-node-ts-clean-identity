fff395431b002512683d3438472c8593df217fdb
Allan Gaby <allan.gaby@gmail.com>
2020-12-04 18:34:52 -0300

    feat: ensure AuthenticationMiddleware use ShowAccount usecase

--- src/presentation/middlewares/auth/authentication-middleware.ts
+++ src/presentation/middlewares/auth/authentication-middleware.ts
@@ -1,5 +1,6 @@
 import { Middleware, HttpRequest, HttpResponse } from '@/presentation/protocols'
-import { ShowAccountBySession, ShowSessionByAccessToken } from '@/domain/usecases/auth/session'
+import { ShowSessionByAccessToken } from '@/domain/usecases/auth/session'
+import { ShowAccount } from '@/domain/usecases/auth/account'
 import { AccountModel, AccountType, SessionType } from '@/domain/models/auth'
 import { badRequest, serverError, ok, forbidden } from '@/presentation/helpers'
 import { MissingParamError } from '@/validation/errors'
@@ -8,7 +9,7 @@ import { AccessDeniedError } from '@/presentation/errors'
 export class AuthenticationMiddleware implements Middleware<any, AccountModel | Error> {
   constructor (
     private readonly showSessionByAccessToken: ShowSessionByAccessToken,
-    private readonly ShowAccountBySession: ShowAccountBySession,
+    private readonly showAccount: ShowAccount,
     private readonly accountType: AccountType[] = null
   ) {}
 
@@ -20,8 +21,8 @@ export class AuthenticationMiddleware implements Middleware<any, AccountModel | 
       }
       const session = await this.showSessionByAccessToken.show({ accessToken })
       if ((session) && (session.type === SessionType.authentication)) {
-        const account = await this.ShowAccountBySession.show({
-          accessToken
+        const account = await this.showAccount.show({
+          accountId: session.account_id
         })
         if ((account) && ((!this.accountType) || (this.accountType.includes(account.type)))) {
           return ok(account)
