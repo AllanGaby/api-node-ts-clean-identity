973e4bbf5fff38645e05f27445b02fee62206d2d
Allan Gaby <allan.gaby@gmail.com>
2020-12-07 08:21:33 -0300

    feat: initial to config swagger

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node TS Identity API',
+    description: 'API de Identificação / Autenticação usando Clean Architecture',
+    version: '1.0.0'
+  }
+}
