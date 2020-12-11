3742cea2585103c6d24d39cde692fa75c730bd5c
Allan Gaby <allan.gaby@gmail.com>
2020-12-10 22:42:50 -0300

    docs: refactor swagger documentation adding contact information

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,12 @@ export default {
   info: {
     title: 'Clean Node TS Identity API',
     description: 'API to identity and authenticate users. Developed using Node JS with Typescript, Clean code and Clean Architecture and without database integration for validation the application domain.',
-    version: '1.0.0'
+    version: '1.0.0',
+    contact: {
+      name: 'Allan Gaby',
+      email: 'allan.gaby@gmail.com',
+      url: 'https://www.linkedin.com/in/allan-gaby'
+    }
   },
   servers: [{
     url: '/api',
