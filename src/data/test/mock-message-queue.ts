40847a1af50666ef2cb1098e683f83c38cdcd21c
Allan Gaby <allan.gaby@gmail.com>
2020-12-13 09:42:08 -0300

    test: ensure SendMail call ConsumeQueue with correct values

--- src/data/test/mock-message-queue.ts
+++ src/data/test/mock-message-queue.ts
@@ -1,4 +1,4 @@
-import { SendToQueue } from '@/data/protocols/message-queue'
+import { SendToQueue, ConsumeQueue } from '@/data/protocols/message-queue'
 
 export class SendToQueueSpy implements SendToQueue {
   params: any
@@ -9,5 +9,15 @@ export class SendToQueueSpy implements SendToQueue {
     this.queueName = queueName
     this.params = params
     return this.result
+  }
+}
+
+export class ConsumeQueueSpy implements ConsumeQueue {
+  queueName: string
+  callback: (params: any) => any
+
+  async consume<ParamsType = any, ResultType = any>(queueName: string, callback: (params: ParamsType) => ResultType): Promise<void> {
+    this.queueName = queueName
+    this.callback = callback
   }
 }
--- src/data/usecases/utils/send-mail.spec.ts
+++ src/data/usecases/utils/send-mail.spec.ts
@@ -1,26 +1,29 @@
 import { DbSendMail } from './send-mail'
-import { SendToQueueSpy, mockSendMailDTO, throwError } from '@/data/test'
+import { SendToQueueSpy, ConsumeQueueSpy, mockSendMailDTO, throwError } from '@/data/test'
 import faker from 'faker'
 
 interface sutTypes {
   sut: DbSendMail
   queueName: string
   sendToQueueSpy: SendToQueueSpy
+  consumeQueueSpy: ConsumeQueueSpy
 }
 
 const makeSut = (): sutTypes => {
   const queueName = faker.database.column()
   const sendToQueueSpy = new SendToQueueSpy()
-  const sut = new DbSendMail(queueName, sendToQueueSpy)
+  const consumeQueueSpy = new ConsumeQueueSpy()
+  const sut = new DbSendMail(queueName, sendToQueueSpy, consumeQueueSpy)
   return {
     sut,
     queueName,
-    sendToQueueSpy
+    sendToQueueSpy,
+    consumeQueueSpy
   }
 }
 
 describe('DbSendMail', () => {
-  test('Should call SendToQueue with correct params', async () => {
+  test('Should call SendToQueue with correct values', async () => {
     const { sut, queueName, sendToQueueSpy } = makeSut()
     const request = mockSendMailDTO()
     await sut.sendMail(request)
@@ -33,5 +36,12 @@ describe('DbSendMail', () => {
     jest.spyOn(sendToQueueSpy, 'sendToQueue').mockImplementationOnce(throwError)
     const promise = sut.sendMail(mockSendMailDTO())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call ConsumeQueue with correct values', async () => {
+    const { sut, queueName, consumeQueueSpy } = makeSut()
+    const request = mockSendMailDTO()
+    await sut.sendMail(request)
+    expect(consumeQueueSpy.queueName).toEqual(queueName)
   })
 })
