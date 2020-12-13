ec401b6f27af756d81a250e7cc2c29423d78038f
Allan Gaby <allan.gaby@gmail.com>
2020-12-13 13:15:42 -0300

    refactor: change SendMailSessionFactory add RabbitMQAdapter

--- src/main/factories/usecases/auth/memory/session/send-mail-session-factory.ts
+++ src/main/factories/usecases/auth/memory/session/send-mail-session-factory.ts
@@ -1,8 +1,11 @@
 import { DbSendMailSession } from '@/data/usecases/auth/session'
 import { MemorySessionRepository } from '@/infra/db/memory/repositories/auth'
 import { makeDbSendMail } from '@/main/factories/usecases/utils'
+import { RabbitMQMessageQueueAdapter } from '@/infra/message-queue'
+import { EnvConfig } from '@/main/config/env'
 
 export const makeDbSendMailSession = (): DbSendMailSession => {
   const sessionRepository = MemorySessionRepository.getInstance()
-  return new DbSendMailSession(sessionRepository, makeDbSendMail())
+  const messageQueueAdapter = new RabbitMQMessageQueueAdapter(EnvConfig.urlRabbitMQ)
+  return new DbSendMailSession(sessionRepository, 'send-mail', messageQueueAdapter, messageQueueAdapter, makeDbSendMail())
 }
