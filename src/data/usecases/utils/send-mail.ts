943068d274112aa8055f3da148c8c25951ff7f8f
Allan Gaby <allan.gaby@gmail.com>
2020-12-13 09:41:50 -0300

    feat: ensure SendMail call ConsumeQueue with correct values

--- src/data/usecases/utils/send-mail.ts
+++ src/data/usecases/utils/send-mail.ts
@@ -1,13 +1,15 @@
 import { SendMail, SendMailDTO, SendMailVariables } from '@/domain/usecases/utils'
-import { SendToQueue } from '@/data/protocols/message-queue'
+import { SendToQueue, ConsumeQueue } from '@/data/protocols/message-queue'
 
 export class DbSendMail implements SendMail {
   constructor (
     private readonly queueName: string,
-    private readonly sendToQueue: SendToQueue
+    private readonly sendToQueue: SendToQueue,
+    private readonly consumeQueue: ConsumeQueue
   ) {}
 
   async sendMail (data: SendMailDTO): Promise<void> {
     this.sendToQueue.sendToQueue<SendMailVariables>(this.queueName, data.variables)
+    this.consumeQueue.consume(this.queueName, () => {})
   }
 }
