5065281b533c9fbad6ef15b85d315e72aa0c1b25
Allan Gaby <allan.gaby@gmail.com>
2020-12-14 15:53:14 -0300

    feat: ensure Execute Method call SendMail Method

--- src/data/usecases/utils/send-mail.spec.ts
+++ src/data/usecases/utils/send-mail.spec.ts
@@ -20,40 +20,52 @@ const makeSut = (): sutTypes => {
 }
 
 describe('DbSendMail', () => {
-  test('Should call MailTemplateAdapter with correct values', async () => {
-    const { sut, mailTemplateAdapterSpy } = makeSut()
-    const request = mockSendMailDTO()
-    await sut.sendMail(request)
-    expect(mailTemplateAdapterSpy.parseParams).toEqual({
-      filePath: request.templateFileName,
-      variables: request.variables
+  describe('SendMail', () => {
+    test('Should call MailTemplateAdapter with correct values', async () => {
+      const { sut, mailTemplateAdapterSpy } = makeSut()
+      const request = mockSendMailDTO()
+      await sut.sendMail(request)
+      expect(mailTemplateAdapterSpy.parseParams).toEqual({
+        filePath: request.templateFileName,
+        variables: request.variables
+      })
+    })
+
+    test('Should return throw if MailTemplateAdapter throws', async () => {
+      const { sut, mailTemplateAdapterSpy } = makeSut()
+      jest.spyOn(mailTemplateAdapterSpy, 'parse').mockImplementationOnce(throwError)
+      const promise = sut.sendMail(mockSendMailDTO())
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('Should call SendMailAdapter with correct values', async () => {
+      const { sut, mailTemplateAdapterSpy, sendMailAdapterSpy } = makeSut()
+      mailTemplateAdapterSpy.mailParsed = faker.random.words()
+      const request = mockSendMailDTO()
+      await sut.sendMail(request)
+      expect(sendMailAdapterSpy.sendMailParams).toEqual({
+        to: request.to,
+        sender: request.sender,
+        subject: request.subject,
+        content: mailTemplateAdapterSpy.mailParsed
+      })
+    })
+
+    test('Should return throw if SendMailAdapter throws', async () => {
+      const { sut, sendMailAdapterSpy } = makeSut()
+      jest.spyOn(sendMailAdapterSpy, 'sendMail').mockImplementationOnce(throwError)
+      const promise = sut.sendMail(mockSendMailDTO())
+      await expect(promise).rejects.toThrow()
     })
   })
 
-  test('Should return throw if MailTemplateAdapter throws', async () => {
-    const { sut, mailTemplateAdapterSpy } = makeSut()
-    jest.spyOn(mailTemplateAdapterSpy, 'parse').mockImplementationOnce(throwError)
-    const promise = sut.sendMail(mockSendMailDTO())
-    await expect(promise).rejects.toThrow()
-  })
-
-  test('Should call SendMailAdapter with correct values', async () => {
-    const { sut, mailTemplateAdapterSpy, sendMailAdapterSpy } = makeSut()
-    mailTemplateAdapterSpy.mailParsed = faker.random.words()
-    const request = mockSendMailDTO()
-    await sut.sendMail(request)
-    expect(sendMailAdapterSpy.sendMailParams).toEqual({
-      to: request.to,
-      sender: request.sender,
-      subject: request.subject,
-      content: mailTemplateAdapterSpy.mailParsed
+  describe('Execute', () => {
+    test('Should call SendMail with correct values', async () => {
+      const { sut } = makeSut()
+      const sendMailSpy = jest.spyOn(sut, 'sendMail')
+      const request = mockSendMailDTO()
+      await sut.execute(request)
+      expect(sendMailSpy).toHaveBeenCalledWith(request)
     })
-  })
-
-  test('Should return throw if SendMailAdapter throws', async () => {
-    const { sut, sendMailAdapterSpy } = makeSut()
-    jest.spyOn(sendMailAdapterSpy, 'sendMail').mockImplementationOnce(throwError)
-    const promise = sut.sendMail(mockSendMailDTO())
-    await expect(promise).rejects.toThrow()
   })
 })
