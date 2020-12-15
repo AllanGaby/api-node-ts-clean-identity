74357d7af3b6d20a3078a98b9b309d8bc7281d25
Allan Gaby <allan.gaby@gmail.com>
2020-12-15 20:45:06 -0300

    test: create CacheRecoverSpy

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -1,4 +1,4 @@
-import { CacheCreate } from '@/data/protocols/cache'
+import { CacheCreate, CacheRecover } from '@/data/protocols/cache'
 
 export class CacheCreateSpy implements CacheCreate {
   key: string
@@ -7,5 +7,15 @@ export class CacheCreateSpy implements CacheCreate {
   async create (key: string, record: object): Promise<void> {
     this.key = key
     this.record = record
+  }
+}
+
+export class CacheRecoverSpy implements CacheRecover {
+  key: string
+  result: any
+
+  async recover<ResultType = any> (key: string): Promise<ResultType> {
+    this.key = key
+    return this.result
   }
 }
