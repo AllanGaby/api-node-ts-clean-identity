24a82009a786f005018e4bdcf381428ccddfbca4
Allan Gaby <allan.gaby@gmail.com>
2020-12-15 21:05:38 -0300

    feat: ensure ShowSessionByAccessToken use CacheAdapter

--- src/data/usecases/auth/session/show-session-by-access-token.ts
+++ src/data/usecases/auth/session/show-session-by-access-token.ts
@@ -3,15 +3,19 @@ import { ShowSessionByAccessToken, ShowSessionByAccessTokenDTO } from '@/domain/
 import { Decrypter } from '@/data/protocols/criptography'
 import { GetSessionByIdRepository } from '@/data/repositories/auth'
 import { InvalidCredentialsError } from '@/data/errors'
+import { CacheCreate, CacheRecover } from '@/data/protocols/cache'
 
 export class DbShowSessionByAccessToken implements ShowSessionByAccessToken {
   constructor (
     private readonly decrypter: Decrypter,
-    private readonly getSessionByIdRepository: GetSessionByIdRepository
+    private readonly getSessionByIdRepository: GetSessionByIdRepository,
+    private readonly cacheRecover: CacheRecover,
+    private readonly cacheCreate: CacheCreate
   ) {}
 
   async show ({ accessToken }: ShowSessionByAccessTokenDTO): Promise<SessionModel> {
     const sessionId = await this.decrypter.decrypt(accessToken)
+    await this.cacheRecover.recover(`session-authentication:${sessionId}`)
     if (sessionId) {
       const session = await this.getSessionByIdRepository.getSessionById(sessionId)
       if (session) {
--- src/main/factories/usecases/auth/memory/session/show-session-by-access-token-factory.ts
+++ src/main/factories/usecases/auth/memory/session/show-session-by-access-token-factory.ts
@@ -2,9 +2,11 @@ import { DbShowSessionByAccessToken } from '@/data/usecases/auth/session'
 import { JWTEncrypterAdapter } from '@/infra/criptografy'
 import { MemorySessionRepository } from '@/infra/db/memory/repositories/auth'
 import { EnvConfig } from '@/main/config/env'
+import { MemoryCacheAdapter } from '@/infra/cache'
 
 export const makeDbShowSessionByAccessToken = (): DbShowSessionByAccessToken => {
   const encrypterAdapter = new JWTEncrypterAdapter(EnvConfig.jwtSecret, 1)
   const sessionRepository = MemorySessionRepository.getInstance()
-  return new DbShowSessionByAccessToken(encrypterAdapter, sessionRepository)
+  const cacheAdapter = new MemoryCacheAdapter()
+  return new DbShowSessionByAccessToken(encrypterAdapter, sessionRepository, cacheAdapter, cacheAdapter)
 }
