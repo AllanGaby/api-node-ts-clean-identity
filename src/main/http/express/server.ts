0cb16ef06dbf5bf7ecf8f0cf8089692bf822659c
Allan Gaby <allan.gaby@gmail.com>
2020-12-22 10:55:41 -0300

    chore: add typeorm and dependencies

--- src/main/http/express/server.ts
+++ src/main/http/express/server.ts
@@ -1,6 +1,7 @@
 import 'module-alias/register'
 import app from '@/main/config/express/app'
 import { EnvConfig } from '@/main/config/env'
+import '@/infra/db/typeorm'
 
 app.listen(EnvConfig.port, () => {
   console.log(`🚀 Server started on port ${EnvConfig.port}!`)
