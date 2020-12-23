84914b17f911ef37b81db73da4845450e10d6876
Allan Gaby <allan.gaby@gmail.com>
2020-12-23 08:36:44 -0300

    test: ensure AccountRepositoryTypeOrm GetAccountByEmail return null if account not found

--- src/infra/db/typeorm/repositories/auth/account-repository.spec.ts
+++ src/infra/db/typeorm/repositories/auth/account-repository.spec.ts
@@ -4,6 +4,7 @@ import { mockCreateAccountDTO } from '@/data/test/auth'
 import { closeConnection, truncateTables } from '@/infra/test/db/typeorm'
 import { createTypeOrmConnection } from '@/infra/db/typeorm/connections'
 import { AccountType } from '@/domain/models/auth'
+import faker from 'faker'
 
 let connection: Connection
 
@@ -44,6 +45,14 @@ describe('AccountRepositoryTypeORM', () => {
       expect(createdAccount.type).toBe(AccountType.student)
       expect(createdAccount.created_at).toBeTruthy()
       expect(createdAccount.updated_at).toBeTruthy()
+    })
+  })
+
+  describe('GetAccountByEmail Method', () => {
+    test('Should return null if account not found', async () => {
+      const { sut } = makeSut()
+      const accountByEmail = await sut.getAccountByEmail(faker.internet.email())
+      expect(accountByEmail).toBeFalsy()
     })
   })
 })
