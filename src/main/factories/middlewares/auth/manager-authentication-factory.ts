ddf13f0cfa53347b693f1d9dbad7f6faf5881d74
Allan Gaby <allan.gaby@gmail.com>
2020-12-24 17:37:53 -0300

    refactor: change ManagerAuthenticationFactory to use module alias

--- src/main/factories/middlewares/auth/manager-authentication-factory.ts
+++ src/main/factories/middlewares/auth/manager-authentication-factory.ts
@@ -1,7 +1,7 @@
 import { AuthenticationMiddleware } from '@/presentation/middlewares/auth'
 import { makeDbShowSessionByAccessToken } from '@/main/factories/usecases/auth/session'
 import { AccountType } from '@/domain/models/auth'
-import { makeDbShowAccount } from '../../usecases/auth/account'
+import { makeDbShowAccount } from '@/main/factories/usecases/auth/account'
 
 export const makeManagerAuthenticationMiddleware = (): AuthenticationMiddleware => {
   return new AuthenticationMiddleware(makeDbShowSessionByAccessToken(), makeDbShowAccount(), [AccountType.manager])
