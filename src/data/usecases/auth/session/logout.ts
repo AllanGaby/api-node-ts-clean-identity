7cca7eeeb8f08918f1836b273eb7c9d30ce152c9
Allan Gaby <allan.gaby@gmail.com>
2021-01-03 14:50:27 -0300

    feat: ensure Logout call CacheRecover with corret values

--- src/data/usecases/auth/session/logout.ts
+++ src/data/usecases/auth/session/logout.ts
@@ -1,15 +1,22 @@
 import { InvalidCredentialsError } from '@/data/errors'
 import { DeleteSessionByIdRepository, GetSessionByIdRepository } from '@/data/repositories/auth'
+import { CacheRecover } from '@/data/protocols/cache'
 import { Logout, LogoutDTO } from '@/domain/usecases/auth/session'
+import { SessionModel } from '@/domain/models/auth'
 
 export class DbLogout implements Logout {
-  constructor (
+  constructor(
     private readonly getSessionByIdRepository: GetSessionByIdRepository,
-    private readonly deleteSessionByIdRepository: DeleteSessionByIdRepository
-  ) {}
+    private readonly deleteSessionByIdRepository: DeleteSessionByIdRepository,
+    private readonly cacheRecover: CacheRecover
+  ) { }
 
-  async logout ({ sessionId }: LogoutDTO): Promise<void> {
-    const session = await this.getSessionByIdRepository.getSessionById(sessionId)
+  async logout({ sessionId }: LogoutDTO): Promise<void> {
+    let session: SessionModel
+    session = await this.cacheRecover.recover(`session-authentication:${sessionId}`)
+    if (!session) {
+      session = await this.getSessionByIdRepository.getSessionById(sessionId)
+    }
     if (session) {
       return await this.deleteSessionByIdRepository.deleteById(session.id)
     }
--- src/main/factories/usecases/auth/session/logout-factory.ts
+++ src/main/factories/usecases/auth/session/logout-factory.ts
@@ -1,9 +1,11 @@
 import { DbLogout } from '@/data/usecases/auth/session'
 import { EnvConfig } from '@/main/config/env'
 import { AuthRepositoriesFactory } from '@/main/factories/repositories'
+import { MemoryCacheAdapter } from '@/infra/cache'
 
 export const makeDbLogout = (): DbLogout => {
   const authRepositoriesFactory = new AuthRepositoriesFactory(EnvConfig.repositoryType)
   const sessionRepository = authRepositoriesFactory.getSessionRepository()
-  return new DbLogout(sessionRepository, sessionRepository)
+  const cacheAdapter = new MemoryCacheAdapter()
+  return new DbLogout(sessionRepository, sessionRepository, cacheAdapter)
 }
