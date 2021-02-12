d6ac9b5e83b8422556fd15ed4c1f34978839f6e8
Allan Gaby <allan.gaby@gmail.com>
2021-02-12 11:10:54 -0300

    test: ensure ShowFileRepository return correct value

--- src/infra/db/memory/repositories/files/file-repository.spec.ts
+++ src/infra/db/memory/repositories/files/file-repository.spec.ts
@@ -1,5 +1,6 @@
 import { MemoryFileRepository } from './file-repository'
 import { mockCreateFileModel } from '@/data/test/files'
+import faker from 'faker'
 
 interface sutTypes {
   sut: MemoryFileRepository
@@ -15,6 +16,23 @@ describe('MemoryFileRepository', () => {
       const { sut } = makeSut()
       const request = mockCreateFileModel()
       const file = await sut.create(request)
+      expect(file.path).toBe(request.filePath)
+    })
+  })
+
+  describe('Show Method', () => {
+    test('Should return undefined if file is not found', async () => {
+      const { sut } = makeSut()
+      const file = await sut.show(faker.random.uuid())
+      expect(file).toBeFalsy()
+    })
+
+    test('Should return a fileModel if file is found', async () => {
+      const { sut } = makeSut()
+      const request = mockCreateFileModel()
+      const createdFile = await sut.create(request)
+      const file = await sut.show(createdFile.id)
+      expect(file).toBeTruthy()
       expect(file.path).toBe(request.filePath)
     })
   })
