b9ae1c94add82a32bf482befb2c8556d2255e6f5
Allan Gaby <allan.gaby@gmail.com>
2021-02-12 18:34:21 -0300

    test: ensure UploadAvatarAccountUseCase call UploadFileUseCase with correct values

--- src/data/usecases/auth/account/upload-avatar-account-use-case.spec.ts
+++ src/data/usecases/auth/account/upload-avatar-account-use-case.spec.ts
@@ -1,6 +1,7 @@
 import { DbUploadAvatarAccountUseCase } from './upload-avatar-account-use-case'
 import { GetAccountByIdRepositorySpy, mockUploadAvatarAccountDTO, throwError } from '@/data/test'
 import { UploadFileUseCaseSpy } from '@/data/test/files'
+import { AccountNotFoundError } from '@/data/errors'
 
 interface sutTypes {
   sut: DbUploadAvatarAccountUseCase
@@ -30,6 +31,29 @@ describe('DbUploadAvatarAccountUseCase', () => {
   test('Should return throw if GetAccountByIdRepository throws', async () => {
     const { sut, getAccountByIdRepository } = makeSut()
     jest.spyOn(getAccountByIdRepository, 'getAccountById').mockImplementationOnce(throwError)
+    const promise = sut.upload(mockUploadAvatarAccountDTO())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return AccountNotFoundError if GetAccountByIdRepository return undefined', async () => {
+    const { sut, getAccountByIdRepository } = makeSut()
+    getAccountByIdRepository.account = undefined
+    const promise = sut.upload(mockUploadAvatarAccountDTO())
+    await expect(promise).rejects.toThrowError(AccountNotFoundError)
+  })
+
+  test('Should call UploadFile with correct values', async () => {
+    const { sut, uploadFileUseCase } = makeSut()
+    const request = mockUploadAvatarAccountDTO()
+    await sut.upload(request)
+    expect(uploadFileUseCase.params).toEqual({
+      filePath: request.avatarFilePath
+    })
+  })
+
+  test('Should return throw if UploadFile throws', async () => {
+    const { sut, uploadFileUseCase } = makeSut()
+    jest.spyOn(uploadFileUseCase, 'upload').mockImplementationOnce(throwError)
     const promise = sut.upload(mockUploadAvatarAccountDTO())
     await expect(promise).rejects.toThrow()
   })
