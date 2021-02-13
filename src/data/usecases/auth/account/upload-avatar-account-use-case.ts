45486f1aaca05f5cec977ce816a565482b6e31fe
Allan Gaby <allan.gaby@gmail.com>
2021-02-13 17:31:04 -0300

    feat: ensure UploadAvatarAccountUseCase call DeleteFileUseCase with correct values

--- src/data/usecases/auth/account/upload-avatar-account-use-case.ts
+++ src/data/usecases/auth/account/upload-avatar-account-use-case.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '@/domain/models/auth'
-import { UploadFileUseCase } from '@/domain/usecases/files'
+import { DeleteFileUseCase, UploadFileUseCase } from '@/domain/usecases/files'
 import { UploadAvatarAccountUseCase, UploadAvatarAccountDTO } from '@/domain/usecases/auth/account'
 import { GetAccountByIdRepository, UpdateAccountRepository } from '@/data/repositories/auth'
 import { AccountNotFoundError } from '@/data/errors'
@@ -7,6 +7,7 @@ import { AccountNotFoundError } from '@/data/errors'
 export class DbUploadAvatarAccountUseCase implements UploadAvatarAccountUseCase {
   constructor (
     private readonly getAccountByIdRepository: GetAccountByIdRepository,
+    private readonly deleteFileUseCase: DeleteFileUseCase,
     private readonly uploadFileUseCase: UploadFileUseCase,
     private readonly updateAccountRepository: UpdateAccountRepository
   ) {}
@@ -15,6 +16,9 @@ export class DbUploadAvatarAccountUseCase implements UploadAvatarAccountUseCase 
     const accountById = await this.getAccountByIdRepository.getAccountById(accountId)
     if (!accountById) {
       throw new AccountNotFoundError()
+    }
+    if (accountById.avatar_file_id) {
+      await this.deleteFileUseCase.delete(accountById.avatar_file_id)
     }
     const file = await this.uploadFileUseCase.upload({
       filePath: avatarFilePath
