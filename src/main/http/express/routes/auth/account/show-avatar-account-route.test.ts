4b69d640fae490c427eee4dd24840dde39faf82d
Allan Gaby <allan.gaby@gmail.com>
2021-02-16 21:31:19 -0300

    test: ensure ShowAvatarAccountRoute return correct avatar file

--- src/main/http/express/routes/auth/account/show-avatar-account-route.test.ts
+++ src/main/http/express/routes/auth/account/show-avatar-account-route.test.ts
@@ -4,7 +4,9 @@ import { MemoryFileRepository } from '@/infra/db/memory/repositories/files'
 import { AccountModel } from '@/domain/models/auth'
 import { HttpStatusCode } from '@/presentation/protocols'
 import { EnvConfig } from '@/main/config/env'
+import { FileModel } from '@/domain/models/files'
 import fs from 'fs'
+import path from 'path'
 import request from 'supertest'
 import faker from 'faker'
 
@@ -12,6 +14,8 @@ let account: AccountModel
 let accountRepository: MemoryAccountRepository
 let fileRepository: MemoryFileRepository
 let accountFilePath: string
+let file: FileModel
+const defaultFilePath = path.resolve(__dirname, '..', '..', '..', '..', '..', '..', '..', EnvConfig.uploadAvatarDir, 'default.png')
 const testFilePath = `${EnvConfig.uploadAvatarDir}/test.jpg`
 const url = '/api/auth/account/avatar/'
 
@@ -19,7 +23,7 @@ describe('GET /avatar/:avatar_id - Show Avatar Account', () => {
   beforeAll(async () => {
     fileRepository = MemoryFileRepository.getInstance()
     accountRepository = MemoryAccountRepository.getInstance()
-    const file = await fileRepository.create({
+    file = await fileRepository.create({
       dir: EnvConfig.uploadAvatarDir,
       extention: '.jpg'
     })
@@ -30,15 +34,26 @@ describe('GET /avatar/:avatar_id - Show Avatar Account', () => {
       email: faker.internet.email(),
       password: faker.internet.password()
     })
-    account.avatar_file_id = file.id
   })
 
   afterAll(async () => {
     fs.promises.unlink(accountFilePath)
   })
 
-  test('Should return ok status code and avatar file', async () => {
-    const response = await request(app).get(`${url}${account.avatar_file_id}`)
+  test('Should return ok status code and default avatar if account without avatar', async () => {
+    const response = await request(app).get(`${url}`)
+    const fileTest = await fs.promises.readFile(defaultFilePath)
+    const buffer = Buffer.from(response.body)
     expect(response.status).toBe(HttpStatusCode.ok)
+    expect(fileTest.toString()).toBe(buffer.toString())
+  })
+
+  test('Should return ok status code and correct avatar if account have avatar', async () => {
+    account.avatar_file_id = file.id
+    const response = await request(app).get(`${url}${account.avatar_file_id}`)
+    const fileTest = await fs.promises.readFile(accountFilePath)
+    const buffer = Buffer.from(response.body)
+    expect(response.status).toBe(HttpStatusCode.ok)
+    expect(fileTest.toString()).toBe(buffer.toString())
   })
 })
