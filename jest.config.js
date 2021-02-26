8e149a4172cdf7ea0fdccf7904bcdb433a125796
Allan Gaby <allan.gaby@gmail.com>
2021-02-26 09:51:29 -0300

    chore: review dirs in jest coverage list

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,15 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/data/usecases/**/*.ts',
     '<rootDir>/src/validation/validations/**/*.ts',
-    '<rootDir>/src/infra/**/repositories/**/*.ts',
-    '<rootDir>/src/infra/criptografy/*.ts',
+    '<rootDir>/src/infra/**',
     '<rootDir>/src/presentation/**/controllers/**/*.ts',
     '<rootDir>/src/presentation/**/middlewares/**/*.ts',
     '<rootDir>/src/main/**/validations/**/*.ts',
+    '!**/tests/**',
+    '!**/migrations/**',
     '!**/protocols/**',
-    '!**/index.ts'
+    '!**/index.ts',
+    '!**/connection.ts'
   ],
   testResultsProcessor: 'jest-sonar-reporter',
   collectCoverage: true,
