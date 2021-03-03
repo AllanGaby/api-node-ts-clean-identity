d066e42fd9439233fe7e3feff167d2ed9c647faa
Allan Gaby <allan.gaby@gmail.com>
2021-03-03 06:44:18 -0300

    refactor: move environment variables to separete file

--- src/main/config/environment/config.ts
+++ src/main/config/environment/config.ts
@@ -2,6 +2,8 @@ import { SMTPConfig } from '@/infra/comunication'
 import { RepositoryTypes } from '@/infra/db'
 import { RedisOptions } from 'ioredis'
 import { CacheTypes } from '@/infra/cache'
+import dotenv from 'dotenv'
+import path from 'path'
 
 export interface User {
   name: string
@@ -23,32 +25,36 @@ export interface Env {
   redisConfig: RedisOptions
 }
 
+dotenv.config({
+  path: path.resolve(__dirname, '..', '..', '..', '..', '.env')
+})
+
 export const EnvConfig: Env = ({
-  port: process.env.PORT || 3333,
-  urlRabbitMQ: process.env.URL_RABBITMQ || 'amqp://identity:masterkey@localhost:5672',
-  baseDir: process.env.BASEDIR || 'src',
-  repositoryType: process.env.REPOSITORY_TYPE as RepositoryTypes || RepositoryTypes.Memory,
-  cacheType: process.env.CACHE_TYPE as CacheTypes || CacheTypes.Memory,
-  jwtSecret: process.env.JWT_SECRET || '01c383ef-b869-43f6-a60a-7b0c1b161d3b',
-  tokenName: process.env.AUTH_TOKEN_NAME || 'x-access-token',
-  uploadAvatarDir: process.env.UPLOAD_AVATAR_DIR || 'uploads/auth/avatar/',
+  port: process.env.PORT,
+  urlRabbitMQ: process.env.URL_RABBITMQ,
+  baseDir: process.env.BASEDIR,
+  repositoryType: process.env.REPOSITORY_TYPE as RepositoryTypes,
+  cacheType: process.env.CACHE_TYPE as CacheTypes,
+  jwtSecret: process.env.JWT_SECRET,
+  tokenName: process.env.AUTH_TOKEN_NAME,
+  uploadAvatarDir: process.env.UPLOAD_AVATAR_DIR,
   default: {
-    name: process.env.DEFAULT_USER_NAME || 'identity',
-    email: process.env.DEFAULT_USER_EMAIL || 'identity@identity.com',
-    password: process.env.DEFAULT_USER_PASSWORD || '$2b$12$YWnAIp/iCkQqSk0cLaRrz.noakEhyzvoKNlIcs6UVeTGy7TXGyQ/2'
+    name: process.env.DEFAULT_USER_NAME,
+    email: process.env.DEFAULT_USER_EMAIL,
+    password: process.env.DEFAULT_USER_PASSWORD
   },
   smtpConfig: {
-    host: process.env.SMTP_HOST || 'smtp.ethereal.email',
-    port: Number(process.env.SMTP_PORT) || 587,
-    secure: Boolean(process.env.SMTP_SECURE) || false,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: Boolean(process.env.SMTP_SECURE),
     auth: {
-      user: process.env.SMTP_USER || 'sammy.gutmann2@ethereal.email',
-      pass: process.env.SMTP_PASS || '6FRHEGTxfJXwt1t5u6'
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
     }
   },
   redisConfig: {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: Number(process.env.REDIS_PORT) || 6379,
-    password: process.env.REDIS_PASSWORD || undefined
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD
   }
 })
